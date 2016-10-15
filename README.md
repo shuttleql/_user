1e963cdc25b06a3fc3a4e59948a276716decbc23
Clement Hoang <clement.hoang24@gmail.com>
2016-10-15 17:47:44 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# User Service #
+
+## Build & Run ##
+
+```sh
+$ cd User_Service
+$ ./sbt
+> jetty:start
+> browse
+```
+
+If `browse` doesn't launch your browser, manually open [http://localhost:8080/](http://localhost:8080/) in your browser.
---
+++ src/main/scala/com/shuttleql/services/user/_UserServlet.scala
@@ -0,0 +1,16 @@
+package com.shuttleql.services.user
+
+import org.scalatra._
+
+class _UserServlet extends UserServiceStack {
+
+  get("/") {
+    <html>
+      <body>
+        <h1>Hello, world!</h1>
+        Say <a href="hello-scalate">hello to Scalate</a>.
+      </body>
+    </html>
+  }
+
+}
