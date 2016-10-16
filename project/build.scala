44101b7e442784d663397f47324cc275911eee36
Clement Hoang <clement.hoang24@gmail.com>
2016-10-16 06:11:50 -0400

    Add slick FRM

--- project/build.scala
+++ project/build.scala
@@ -31,7 +31,11 @@ object UserServiceBuild extends Build {
         "org.eclipse.jetty" % "jetty-webapp" % "9.2.15.v20160210" % "container",
         "javax.servlet" % "javax.servlet-api" % "3.1.0" % "provided",
         "org.scalatra" %% "scalatra-json" % ScalatraVersion,
-        "org.json4s"   %% "json4s-jackson" % "3.3.0"
+        "org.json4s" %% "json4s-jackson" % "3.3.0",
+        "com.typesafe.slick" %% "slick" % "3.1.1",
+        "org.slf4j" % "slf4j-nop" % "1.6.4",
+        "org.postgresql" % "postgresql" % "9.4.1211",
+        "com.typesafe.slick" %% "slick-hikaricp" % "3.1.1"
       ),
       scalateTemplateConfig in Compile <<= (sourceDirectory in Compile){ base =>
         Seq(
